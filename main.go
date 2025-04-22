625d4daf29caa79bcd0380c1aa42de3d567e3b1d
Metodija Antuleski <meto.a@beyondbasics.co>
2025-04-22 17:08:51 +0200

    initcommit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	printer := &cobra.Command{
+		Use:   "printer",
+		Short: "printer - simple CLI that prints the first argument",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) < 1 {
+				fmt.Println("Please provide an argument.")
+				return
+			}
+			fmt.Println(args[0])
+		},
+	}
+
+	if err := printer.Execute(); err != nil {
+		fmt.Println("something went wrong")
+		os.Exit(1)
+	}
+}
